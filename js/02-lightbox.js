d933222e7316d6b1d57698961dd19254b0d40b3d
RomaSamkov <Romansamkov.rs@gmail.com>
2022-04-04 21:26:12 +0300

    add task-2 to lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryImagesContainerEl = document.querySelector('.gallery');
+const itemsMurkup = createItemsImagesMurkup(galleryItems);
+galleryImagesContainerEl.insertAdjacentHTML('beforeend', itemsMurkup);
+
+function createItemsImagesMurkup(item) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<ul class="gallery">
+      <li>
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"          
+          alt="${description}"
+        />
+      </a>
+      </li>
+     </ul>`
+  }).join('');
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
